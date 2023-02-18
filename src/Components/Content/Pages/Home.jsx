a0e1ef5f5e6b7c2e6ffd7dbd8a241cb00a7ae6fc
Roman-cas88 <orekhovskiy.roman@gmail.com>
2023-02-18 08:43:07 +0100

    home photo

--- src/Components/Content/Pages/Home.jsx
+++ src/Components/Content/Pages/Home.jsx
@@ -3,8 +3,8 @@ import { Container } from 'react-bootstrap'
 
 export const Home = () => {
   return (
-    <div  className='pages' style={{backgroundColor:"grey"}} id="home">
-      <Container>
+    <div  className='pages' id="home">
+      <Container className=''>
               <h1>Hello, my name is Roman Orekhovskyi</h1>
               <h2>This is my own website about me</h2> 
       </Container>
