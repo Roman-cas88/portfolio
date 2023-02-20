5132dc7eaf0691c7a8a160fd9075020bf64321a5
Roman-cas88 <orekhovskiy.roman@gmail.com>
2023-02-20 18:49:02 +0100

    Update Expirience.jsx

--- src/Components/Content/Pages/Expirience.jsx
+++ src/Components/Content/Pages/Expirience.jsx
@@ -5,7 +5,26 @@ export const Expirience = () => {
   return (
     <div className='pages' id='expirience' style={{backgroundColor:"green"}}>
       <Container>
-          Expirience
+          <h1>Expirience</h1>
+          <p>
+            Sept 2010 – Feb 2022
+            Construction of industrial facilities in the territories of "ArcelorMittal Kryvyi Rih", "Metinvest".
+
+            - Development and optimization of work schedules in accordance with the deadlines.
+            - Building 3D models of the object for a more detailed study of the project and minimizing errors during the execution of work.
+            - Implementation of a cloud database with materials and equipment of the company for easy control by project participants.
+            - Communication with customers and vendors.
+            - Teamwork.
+            - Making decisions at critical moments.
+            - Planning and structuring your working time.
+            - Delegation, for the possibility of parallel execution of priority tasks.
+            - Interviewing, hiring and training new employees.
+          </p>
+          <p>
+            Sept 2022 - Present
+            Fredrick Lindström Bygg & VVS AB.
+            Interior renovations. Builder. Stockholm, Sweden
+          </p>
       </Container>
     </div>
   )
