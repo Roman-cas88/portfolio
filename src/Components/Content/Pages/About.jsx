d97688ea60f8642f82f77e36b1c504928e01cdd0
Roman-cas88 <orekhovskiy.roman@gmail.com>
2023-02-20 12:43:02 +0100

    Update About.jsx

--- src/Components/Content/Pages/About.jsx
+++ src/Components/Content/Pages/About.jsx
@@ -5,7 +5,22 @@ export const About = () => {
   return (
     <div className='pages' style={{backgroundColor:"yellow"}} id='about'>
       <Container>
-          About Me
+      My name is Roman Orekhovskyi. I came to Sweden from Ukraine in July.
+
+I have a higher academic education for a specialist degree in project and program management. Studying at this faculty allowed me to acquire versatile theoretical knowledge, as well as get acquainted with HTML and CSS.
+
+Unfortunately, due to the Russian invasion of Ukraine, my family and I were forced to leave our home and work in Ukraine. Since I am particularly interested in IT development, I would like to take this opportunity to change careers and enter the world of technology as a front-end developer.
+
+Every day I spend a lot of time on self-education to get up-to-date knowledge and professional skills such as HTML, CSS, JavaScript, React.js.
+
+Since I'm new to Sweden and open to new challenges, I'm currently looking for a job or internship as a junior front-end developer.
+
+I am a sociable, responsible and very easy to train person. I work well both on my own and in a team.
+In my free time, I love learning new things and spending time with my family.
+
+I am sure that I will be useful to the company, last two years in Ukraine I have worked in non-standard conditions for me, completing projects that are not familiar to me from the technical side, but I did not give up and always found solutions to problems by communicating with customers, specialists in this field and reading the documentation.
+ 
+
       </Container>
     </div>
   )
