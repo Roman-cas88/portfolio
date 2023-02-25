7a95ff9654a6672e1f45372adcd661c1670f2b70
Roman-cas88 <orekhovskiy.roman@gmail.com>
2023-02-25 10:22:30 +0100

    style navbar link buttons

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,52 +7,46 @@ export const Header = () => {
     <div>
         <Navbar className='bg-secondary' fixed='top'>
         <Container>
-          <Navbar.Brand href="#home" className='text-white'>Roman Orekhovskyi</Navbar.Brand>
+          <Navbar.Brand href="#home" className='logo'>Roman Orekhovskyi</Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
-          <Nav className="justify-content-end flex-grow-1 pe-3">
-            <NavLink>
+          <Nav className="justify-content-end flex-grow-1">
               <Link to='home'
                     activeClass="active" 
-                    spy={true}>
+                    spy={true}
+                    >
                 Home
               </Link>
-            </NavLink>
-            <NavLink>
               <Link to='about'
                     activeClass="active" 
-                    spy={true}>
+                    spy={true}
+                    >
                 About me
               </Link>
-            </NavLink>
-            <NavLink>
               <Link to='education'
                     activeClass="active" 
-                    spy={true}>
+                    spy={true}
+                    >
                 My education
               </Link>
-            </NavLink>
-            <NavLink>
               <Link to='expirience'
                       activeClass="active" 
-                      spy={true}>
+                      spy={true}
+                      >
                 My experience
               </Link>
-            </NavLink>
-            <NavLink>
               <Link to='portfolio'
                       activeClass="active" 
-                      spy={true}>
+                      spy={true}
+                      >
                 Portfolio
               </Link>
-            </NavLink>
-            <NavLink>
               <Link to='contact'
                       activeClass="active" 
-                      spy={true}>
+                      spy={true}
+                      >
                 Contact
               </Link>
-            </NavLink>
           </Nav>
           </Navbar.Collapse>
         </Container>
